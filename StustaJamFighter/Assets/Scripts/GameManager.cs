34b0d82bab32132f3f12760022eaa29830f26e51
Max <tharr@in.tum.de>
2015-03-01 17:08:53 +0100

    gamemanager singleton setup

--- StustaJamFighter/Assets/Scripts/GameManager.cs
+++ StustaJamFighter/Assets/Scripts/GameManager.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	//Singleton routine
+	public GameManager instance;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else
+			Destroy (gameObject);
+		DontDestroyOnLoad (gameObject);
 	}
 	
 	// Update is called once per frame
