917d38059b76dce67659d16551a864e613431e5f
kindl <JanoschKindl@web.de>
2015-03-01 20:30:00 +0100

    provisional punch

--- StustaJamFighter/Assets/Scripts/InputTemp.cs
+++ StustaJamFighter/Assets/Scripts/InputTemp.cs
@@ -6,8 +6,12 @@ using System.Collections;
 public class InputTemp : MonoBehaviour {
 
     public Player player;
+    public GameObject fist;
     public float speed = 10.0f;
     public float jump = 0.5f;
+
+    public Vector3 fistStartPosition;
+    public Vector3 fistPunchPosition;
 
 	// Use this for initialization
 	void Start () {
@@ -17,5 +21,13 @@ public class InputTemp : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         player.transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") * jump, 0.0f) * (Time.deltaTime * speed));
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            fist.transform.localPosition = fistPunchPosition;
+        }
+        else
+        {
+            fist.transform.localPosition = fistStartPosition;
+        }
 	}
 }
