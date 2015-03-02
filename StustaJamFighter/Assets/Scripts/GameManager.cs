66e4ef8821edbb29a310f77e9ec853dd5fd1e2c8
Langbein <langbeia@in.tum.de>
2015-03-02 22:54:02 +0100

    - Respawn -

--- StustaJamFighter/Assets/Scripts/GameManager.cs
+++ StustaJamFighter/Assets/Scripts/GameManager.cs
@@ -5,8 +5,16 @@ public class GameManager : MonoBehaviour {
 
 	//Singleton instance
 	public static GameManager instance;
+	
+	public Transform Player1Prefab;
+	public Transform Player2Prefab;
+	
+	//Wie lange bis ein neuer Gegner gespawned wird
+	public float DyingTimer = 2.0f;
 
     public Player[] players;
+    
+    bool RoutineRunning = false;
 
 	// Use this for initialization
 	void Awake () {
@@ -25,7 +33,37 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		
+		for( int i = 0; i < players.Length; i++ )
+		{
+			if(players[i] == null && RoutineRunning == false)
+			{
+				StartCoroutine("CoolDown",new Vector2(DyingTimer,i));
+				
+			}
+		}
 	
+	}
+	
+	IEnumerator CoolDown(Vector2 DataVec)
+	{
+		float WaitingTime = DataVec.x;
+		int Index = (int)DataVec.y;
+		
+		RoutineRunning = true;
+		if(Index == 0)
+		{
+			yield return new WaitForSeconds(WaitingTime);
+			Transform NuPlayer = Instantiate(Player2Prefab,new Vector3(0,0,0),Quaternion.identity) as Transform;
+			players[Index] = NuPlayer.GetComponent<Player>();
+			players[1].Enemy = players[Index];
+		}
+		else
+		{
+			yield return new WaitForSeconds(WaitingTime);
+			Transform NuPlayer = Instantiate(Player1Prefab,new Vector3(0,0,0),Quaternion.identity) as Transform;
+			players[Index] = NuPlayer.GetComponent<Player>();
+			players[0].Enemy = players[Index];
+		}
+		RoutineRunning = false;
 	}
 }
--- StustaJamFighter/Assets/Scripts/Hauptmenu.cs
+++ StustaJamFighter/Assets/Scripts/Hauptmenu.cs
@@ -65,7 +65,6 @@ public class Hauptmenu : MonoBehaviour {
                 {
                     LoadLevel1();
                 }
-
         }
 	
 	
