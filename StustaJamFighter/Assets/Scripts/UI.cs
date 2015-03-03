9a2ecb5152d9a1fd48e4caa7529470d25fc52a83
kindl <JanoschKindl@web.de>
2015-03-03 11:22:16 +0100

    los healthbaros grandes

--- StustaJamFighter/Assets/Scripts/UI.cs
+++ StustaJamFighter/Assets/Scripts/UI.cs
@@ -3,10 +3,8 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class UI : MonoBehaviour {
-    public GameObject healthBarLeft;
-    public GameObject healthBarRight;
-
-    public float healthBarScaling = 0.1f;
+    public RectTransform healthBarLeft;
+    public RectTransform healthBarRight;
 
 	// Use this for initialization
 	void Start () {
@@ -14,16 +12,15 @@ public class UI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
 		//Debug.Log (GameManager.instance.players[1].health+" "+GameManager.instance.players[0].health);
 	
 		if(healthBarLeft != null)
 		{
-        	healthBarLeft.transform.localScale = new Vector3(GameManager.instance.players[1].health * healthBarScaling, 1.0f, 1.0f);
+        	healthBarLeft.sizeDelta = new Vector2(GameManager.instance.players[1].health, 100.0f);
         }
         if(healthBarRight != null)
         {
-        	healthBarRight.transform.localScale = new Vector3(GameManager.instance.players[0].health * healthBarScaling, 1.0f, 1.0f);
+            healthBarRight.sizeDelta = new Vector2(GameManager.instance.players[0].health, 100.0f);
 		}
 	}
 }
