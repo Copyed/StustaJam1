2f4fac5f9c9fd9d611a09f55ea9c82ae3d4b4581
Langbein <langbeia@in.tum.de>
2015-03-03 13:49:52 +0100

    - Secret "H" EasterEgg

---
+++ StustaJamFighter/Assets/Scripts/HootSurprise.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HootSurprise : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+	
+		anim = this.GetComponent<Animator>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	
+		
+		if(Input.GetButtonDown("Y"))
+		{
+			Debug.Log ("HOOT");
+			anim.SetTrigger("Hoot");
+		}
+	
+	}
+}
